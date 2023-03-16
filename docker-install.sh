71e0ade2e0f46c55d0f9a6a9da99c663a6d7e05e
luudinhmac <nguoiemcuanui@gmail.com>
2023-03-16 15:12:05 +0700

    update docker-compose

--- docker-install.sh
+++ docker-install.sh
@@ -10,11 +10,13 @@
 # Check distro linux
 is_ubuntu=$(awk -F '=' '/PRETTY_NAME/ { print $2 }' /etc/os-release | egrep Ubuntu -i)
 is_centos=$(awk -F '=' '/PRETTY_NAME/ { print $2 }' /etc/os-release | egrep CentOS -i)
-
+is_amazon_linux=$(awk -F '=' '/PRETTY_NAME/ { print $2 }' /etc/os-release | egrep 'Amazon Linux 2' -i)
 # Get docker exist
 if [ ! -z "$is_ubuntu" ]; then
     is_docker_exist=$(dpkg -l | grep docker -i)
 elif [ ! -z "$is_centos" ]; then
+    is_docker_exist=$(rpm -qa | grep docker)
+elif [ ! -z "$is_amazon_linux" ]; then
     is_docker_exist=$(rpm -qa | grep docker)
 else
     echo "Error: Current Linux release version is not supported, please use either centos or ubuntu. "
@@ -67,6 +69,20 @@ function centos_install() {
     echo "Docker has been installed successfully."
 }
 
+function amazon_install() {
+    sudo yum install -y git wget curl
+    sudo amazon-linux-extras install docker -y
+    sudo systemctl enable docker
+    sudo systemctl start docker
+    is_docker_success=$(sudo docker run hello-world | grep -i "Hello from Docker")
+    if [ -z "$is_docker_success" ]; then
+        echo -e "Error: Docker installation Failed."
+        exit
+    fi
+    echo "Docker has been installed successfully."
+
+}
+
 # Install docker-compose
 function docker_compose_install() {
     COMPOSE_VERSION=v$(git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oE "[0-9]+\.[0-9][0-9]+\.[0-9]+$" | sort --version-sort | tail -n 1)
@@ -74,11 +90,14 @@ function docker_compose_install() {
     sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m) > /usr/local/bin/docker-compose"
     sudo chmod +x /usr/local/bin/docker-compose
     sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
+    sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 }
 if [ ! -z "$is_ubuntu" ]; then
     ubuntu_install
 elif [ ! -z "$is_centos" ]; then
     centos_install
+elif [ ! -z "$is_amazon_linux" ]; then
+    amazon_install
 fi
 docker_compose_install
 docker-compose --version
